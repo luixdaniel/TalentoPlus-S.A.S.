17c6e956b6f933adba13727f73a2d9c74c5fac4c
luisxdaniel <ceraluis4@gmail.com>
2025-12-09 14:12:34 -0500

    create projec

---
+++ TalentoPlus S.A.S.ll.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TalentoPlus_S.A.S.ll.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
